0f3fcfea3d92f54f66329f3f9f073d6d56d20cb8
throsturg <throsturg@gmail.com>
2015-06-19 21:42:09 +0000

    Functions implemented

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,44 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  
+  get <- function()
+  {
+    x
+  }
+  
+  set <- function(y)
+  {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  getInverse <- function()
+  {
+    inv
+  }
+  setInverse <- function(inverseMatrix)
+  {
+    inv <<- inverseMatrix
+  }
+  
+  list(set = set, get = get, getInverse = getInverse, setInverse = setInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)) 
+  {
+    message("Using Cache")
+    return(inv)
+  }
+  message("Crate Cache")
+  mat <- x$get()
+  inv <- solve(mat)
+  x$setInverse(inv)
+  inv
 }
